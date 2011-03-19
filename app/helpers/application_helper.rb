c7a9ceb1118dd4a4df395f0658fac0a6d914b483
David Sauvage <davidsauvage8@gmail.com>
2011-03-19 11:14:17 +0100

    Review the specs for helpers
      -remove specs that we did'nt needed
      -write the others

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,35 +1,35 @@
 # -*- coding: utf-8 -*-
 
 module ApplicationHelper
-    def signin_link
-        user = session[:user]
-        if user.nil? then
-            return link_to("Se connecter", signin_path, :class => "lienmenu")
-        else
-            link = "#{link_to 'Se déconnecter', signout_path, :method => :delete, :class => "lienmenu"}"
-            return link.html_safe
-        end
-    end    
 
-	def global_title
-		base_title = "Bienvenue !"
-		if @title.nil? then
-			return base_title
-		else 
-			return "#{@title}"
-		end
-	end    
+  def signin_link
+    user = session[:user]
+    if user.nil? then
+      return link_to("Se connecter", signin_path, :class => "lienmenu")
+    else
+      link = "#{link_to 'Se déconnecter', signout_path, :method => :delete, :class => "lienmenu"}"
+      return link.html_safe
+    end
+  end
 
-   def link_to_remove_fields(name, f)  
-      f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")  
-   end 
+  def global_title
+    base_title = "Bienvenue !"
+    if @title.nil? then
+      return base_title
+    else
+      return "#{@title}"
+    end
+  end
 
-   def link_to_add_fields(name, f, association) 
-      new_object = f.object.class.reflect_on_association(association).klass.new  
-      fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|  
-      render(association.to_s.singularize + "_fields", :f => builder)  
-      end  
-      link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")  
-   end  
+  def link_to_remove_fields(name, f)
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+  end
 
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
+    end
+    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+  end
 end
--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -11,5 +11,28 @@ require 'spec_helper'
 #   end
 # end
 describe SessionsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    u = User.create(:name => "admin", :email => "admin@a.com", :password => "admin", :admin => true)
+    session[:user] = u
+  end
+
+  describe "signout" do
+    it "deletes the user of the session" do
+      signout
+      session[:user].should == nil
+    end
+  end
+
+  describe "session_admin?" do
+    it "should return true when it's an admin" do
+      session_admin?.should == true
+    end
+    it "should return false when it's not an admin" do
+      signout
+      u = User.create(:name => "toto", :email => "toto@a.com", :password => "toto", :admin => false)
+      session[:user] = u
+      session_admin?.should == false
+    end
+  end
+
 end
