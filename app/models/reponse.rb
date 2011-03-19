15d37e622beda421f88ab74b3882998d26ef280d
David Sauvage <davidsauvage8@gmail.com>
2011-03-19 10:49:47 +0100

    Fix the reponse model
      -Since the model has some attr_accessible we needed to
       give :title to make specs work

--- app/models/reponse.rb
+++ app/models/reponse.rb
@@ -3,7 +3,7 @@ class Reponse < ActiveRecord::Base
   belongs_to :question
   
   attr_accessor :rep
-  attr_accessible :rep
+  attr_accessible :rep, :title
 
   validates_presence_of :question, :message => "La réponse doit appartenir à une question"
   validates_presence_of :title, :message => "Réponse obligatoire"
